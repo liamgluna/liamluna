9e4484fe20abdae9f70350afe8fec3de443bce47
Liam Luna <liamgluna@gmail.com>
2025-04-01 20:50:39 +0800

    add margin

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-800 text-white">
       <div className="mx-2 text-center font-mono">
-        <p className="text-2xl font-bold underline sm:text-4xl">Liam Luna</p>
+        <p className="mb-2 text-2xl font-bold underline sm:text-4xl">
+          Liam Luna
+        </p>
         <p className="text-xs sm:text-xl">
           Software Engineer | Backend | Go | TypeScript
         </p>
