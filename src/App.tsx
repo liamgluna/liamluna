6359d753b29789928a4c96b19b29ef1a627bc935
Liam Luna <liamgluna@gmail.com>
2025-05-25 16:35:00 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-800 text-zinc-100">
+      <div className="mx-2 text-center font-mono">
+        <p className="mb-2 text-2xl font-bold underline sm:text-4xl">Liam Luna</p>
+        <p className="text-xs sm:text-xl">Software Engineer | Backend | Go | TypeScript</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
